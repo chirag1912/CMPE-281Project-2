c3028fd9d69e00f8cac5665d55ef53ff87593151
shivanipandit88 <89808009+shivanipandit88@users.noreply.github.com>
2021-11-12 14:21:40 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+const App = () => (
+  <div>
+    <AmplifySignOut />
+    My App
+  </div>
+);
+
+export default withAuthenticator(App);
